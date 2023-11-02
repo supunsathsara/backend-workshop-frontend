887c0295eb7380a4afb3dd6c2a383884abcbb7f5
Savindu Supun <savindusupunsathsara@gmail.com>
2023-11-02 16:22:03 +0530

    impl login & logout ✨

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 
 
 function Auth() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const getUserOnLoad = async () => {
+
+            const { data: { user } } = await supabase.auth.getUser();
+            console.log(user)
+            setUser(user)
+        }
+
+        getUserOnLoad()
+    }, [])
+
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://efvuymwccuxpqcsbrhhl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmdnV5bXdjY3V4cHFjc2JyaGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4NTMwNjMsImV4cCI6MjAxNDQyOTA2M30.gWZQJLYktfF8v5qzVZtaPEyAY_r5n77WXubgfKooBpE')
 
@@ -20,10 +33,37 @@ function Auth() {
         console.log(error)
     }
 
+    const handleLogin = async () => {
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: 'test00@supunsathsara.com',
+            password: '123@321',
+        })
+
+        console.log(data)
+    }
+
+    const handleLogout = async () => {
+        const { error } = await supabase.auth.signOut()
+        console.log(error)
+    }
+
+    const getUser = async () => {
+
+        const { data: { user } } = await supabase.auth.getUser();
+        console.log(user)
+        setUser(user)
+    }
+
     return (
-        <div className='text-white text-2xl flex flex-row mx-auto text-center gap-6'>
+        <div className='text-white text-2xl flex flex-col mx-auto text-center gap-6'>
             <button onClick={handleRegister}>Register</button>
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <button onClick={getUser}>Get User</button>
+
+            {user && <p>{user.email}</p>}
+            {!user && <p>Not Logged in</p>}
         </div>
     )
 }
